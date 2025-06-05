1fa40db9bdc5aaafdc770d2a5dd2e740da014bad
Bob Ross <Bobross@nowhere.com>
2025-06-05 19:55:59 +1000

    Clean up code

--- main.py
+++ main.py
@@ -1,73 +1,58 @@
 from machine import Pin, I2C, ADC
 import time
 
-# TCS34725 constants
-TCS34725_ADDRESS = 0x29
-COMMAND_BIT = 0x80
+# The color sensor level for detecting the presence of a ball
+BALL_DETECTED_THRESHOLD = 3000
 
-ENABLE = 0x00
-ATIME = 0x01
-CONTROL = 0x0F
-ID = 0x12
-CDATA = 0x14
+# I2C pins
+I2C_0_SDA = 4
+I2C_0_SCL = 5
+I2C_1_SDA = 18
+I2C_1_SCL = 19
 
-ENABLE_PON = 0x01
-ENABLE_AEN = 0x02
-
-GAIN_1X = 0x00
-GAIN_4X = 0x01
-GAIN_16X = 0x02
-
-# PCA9685 constants
-PCA9685_ADDR = 0x40
-MODE1 = 0x00
-PRESCALE = 0xFE
-LED0_ON_L = 0x06
-
-# Stepper outputs (map to stepper driver inputs)
-STEPPERI_IN1 = 10
-STEPPERI_IN2 = 11
-STEPPERI_IN3 = 12
-STEPPERI_IN4 = 13
-
+# Hall effect sensor pin number
 STEPPER_HOME_SENSOR_PIN = 26
+
+# The level threshold to detect magnet near hall effect sensor
 STEPPER_HOME_THRESHOLD = 25000
 
 # Ball toggle angles
-MIN_ANGLE = 0
-MAX_ANGLE = 55
+BALL_TOGGLE_MIN_ANGLE = 0
+BALL_TOGGLE_MAX_ANGLE = 55
+
+# Stepper position to direct to each tray
+TRAY_1_DIRECT_POS = 4096 - 170
+TRAY_2_DIRECT_POS = 170
+
+# Stepper directions (clockwise/counterclockwise)
+STEPPER_CCW = -1
+STEPPER_CW = 1
 
 
-class Motor:
-    stepms = 10
+class StepperMotor:
+    # Stepper outputs (map to stepper driver inputs)
+    STEPPER_IN1 = 10
+    STEPPER_IN2 = 11
+    STEPPER_IN3 = 12
+    STEPPER_IN4 = 13
 
-    # Do be defined by subclasses
-    maxpos = 0
-    states = []
+    stepms = 3
+    maxpos = 4096
+    states = [
+        [1, 0, 0, 0],
+        [1, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 1, 0],
+        [0, 0, 1, 0],
+        [0, 0, 1, 1],
+        [0, 0, 0, 1],
+        [1, 0, 0, 1],
+    ]
 
-    def __init__(self, p1, p2, p3, p4, stepms=None):
-        self.pins = [p1, p2, p3, p4]
-
-        if stepms is not None:
-            self.stepms = stepms
-
+    def __init__(self):
+        self.pins = [Pin(self.STEPPER_IN1, Pin.OUT), Pin(self.STEPPER_IN2, Pin.OUT), Pin(self.STEPPER_IN3, Pin.OUT), Pin(self.STEPPER_IN4, Pin.OUT)]
         self._state = 0
         self._pos = 0
-
-    def __repr__(self):
-        return '<{} @ {}>'.format(
-            self.__class__.__name__,
-            self.pos,
-        )
-
-    @property
-    def pos(self):
-        return self._pos
-
-    @classmethod
-    def frompins(cls, *pins, **kwargs):
-        return cls(*[Pin(pin, Pin.OUT) for pin in pins],
-                   **kwargs)
 
     def reset(self):
         self._pos = 0
@@ -117,110 +102,118 @@ class Motor:
 
         self.pins_off()
 
-    def step_until_angle(self, angle, dir=None):
-        if angle < 0 or angle > 360:
-            raise ValueError(angle)
+class Pca9685:
+    DEVICE_ADDR = 0x40
+    MODE1 = 0x00
+    PRESCALE = 0xFE
+    LED0_ON_L = 0x06
 
-        target = int(angle / 360 * self.maxpos)
-        self.step_until(target, dir=dir)
-
-    def step_degrees(self, degrees):
-        if degrees < 0 or degrees > 360:
-            raise ValueError("Degrees should be between 0 and 360")
-
-        steps_to_take = int(degrees / 360 * self.maxpos)
-
-        self.zero()  # Ignore the current position, start from zero
-        self.step(steps_to_take)
+    def __init__(self, i2c):
+        self.i2c = i2c
+        self.i2c.writeto_mem(self.DEVICE_ADDR, self.MODE1, b'\x00')  # normal mode
+        self.set_pwm_freq(50)  # 50Hz for servo
 
 
-class HalfStepMotor(Motor):
-    stepms = 3
-    maxpos = 4096
-    states = [
-        [1, 0, 0, 0],
-        [1, 1, 0, 0],
-        [0, 1, 0, 0],
-        [0, 1, 1, 0],
-        [0, 0, 1, 0],
-        [0, 0, 1, 1],
-        [0, 0, 0, 1],
-        [1, 0, 0, 1],
-    ]
+    def set_pwm_freq(self, freq_hz):
+        prescale_val = int(25000000.0 / (4096 * freq_hz) - 1)
+        old_mode = self.i2c.readfrom_mem(self.DEVICE_ADDR, self.MODE1, 1)[0]
+        sleep_mode = (old_mode & 0x7F) | 0x10
+        self.i2c.writeto_mem(self.DEVICE_ADDR, self.MODE1, bytes([sleep_mode]))  # go to sleep
+        self.i2c.writeto_mem(self.DEVICE_ADDR, self.PRESCALE, bytes(
+            [prescale_val]))  # set prescale
+        self.i2c.writeto_mem(self.DEVICE_ADDR, self.MODE1, bytes([old_mode]))  # wake up
+        time.sleep_ms(5)
+        self.i2c.writeto_mem(self.DEVICE_ADDR, self.MODE1, bytes(
+            [old_mode | 0xA1]))  # auto-increment
 
 
-def pca9685_init():
-    i2c_1.writeto_mem(PCA9685_ADDR, MODE1, b'\x00')  # normal mode
-    pca9685_set_pwm_freq(50)  # 50Hz for servo
+    def set_pwm(self, channel, on, off):
+        reg = self.LED0_ON_L + 4 * channel
+        data = bytes([on & 0xFF, on >> 8, off & 0xFF, off >> 8])
+        self.i2c.writeto_mem(self.DEVICE_ADDR, reg, data)
+
+    def set_servo_us(self, channel, us):
+        # 1 tick = (1/50Hz) / 4096 = ~4.88us
+        ticks = int(us * 4096 / 20000)
+        self.set_pwm(channel, 0, ticks)
 
 
-def pca9685_set_pwm_freq(freq_hz):
-    prescale_val = int(25000000.0 / (4096 * freq_hz) - 1)
-    old_mode = i2c_1.readfrom_mem(PCA9685_ADDR, MODE1, 1)[0]
-    sleep_mode = (old_mode & 0x7F) | 0x10
-    i2c_1.writeto_mem(PCA9685_ADDR, MODE1, bytes([sleep_mode]))  # go to sleep
-    i2c_1.writeto_mem(PCA9685_ADDR, PRESCALE, bytes(
-        [prescale_val]))  # set prescale
-    i2c_1.writeto_mem(PCA9685_ADDR, MODE1, bytes([old_mode]))  # wake up
-    time.sleep_ms(5)
-    i2c_1.writeto_mem(PCA9685_ADDR, MODE1, bytes(
-        [old_mode | 0xA1]))  # auto-increment
+    def set_servo_angle(self, channel, angle):
+        min_us = 500
+        max_us = 2500
+        delta_us = max_us - min_us
+        us_per_degree = delta_us / 180
+        us = angle * us_per_degree + min_us
+        self.set_servo_us(channel, us)
 
 
-def pca9685_set_pwm(channel, on, off):
-    reg = LED0_ON_L + 4 * channel
-    data = bytes([on & 0xFF, on >> 8, off & 0xFF, off >> 8])
-    i2c_1.writeto_mem(PCA9685_ADDR, reg, data)
+class Tcs34725:
+    DEVICE_ADDRESS = 0x29
+    COMMAND_BIT = 0x80
 
-# --- Servo Control ---
+    ENABLE = 0x00
+    ATIME = 0x01
+    CONTROL = 0x0F
+    ID = 0x12
+    CDATA = 0x14
 
+    ENABLE_PON = 0x01
+    ENABLE_AEN = 0x02
 
-def pca9685_set_servo_us(channel, us):
-    # 1 tick = (1/50Hz) / 4096 = ~4.88us
-    ticks = int(us * 4096 / 20000)
-    pca9685_set_pwm(channel, 0, ticks)
+    GAIN_1X = 0x00
+    GAIN_4X = 0x01
+    GAIN_16X = 0x02
 
+    def __init__(self, i2c):
+        self.i2c = i2c
 
-def pca9685_set_servo_angle(channel, angle):
-    min_us = 500
-    max_us = 2500
-    delta_us = max_us - min_us
-    us_per_degree = delta_us / 180
-    us = angle * us_per_degree + min_us
-    pca9685_set_servo_us(channel, us)
+        self.write8(self.ENABLE, self.ENABLE_PON)
+        time.sleep(0.01)
+        self.write8(self.ENABLE, self.ENABLE_PON | self.ENABLE_AEN)
 
+        self.write8(self.ATIME, 0x00)         # Max integration time
+        self.write8(self.CONTROL, self.GAIN_4X)    # Set gain
 
-def tcs34725_write8(reg, val):
-    i2c_0.writeto_mem(TCS34725_ADDRESS, COMMAND_BIT | reg, bytes([val]))
+    def write8(self, reg, val):
+        self.i2c.writeto_mem(self.DEVICE_ADDRESS, self.COMMAND_BIT | reg, bytes([val]))
 
-
-def tcs34725_read8(reg):
-    return int.from_bytes(i2c_0.readfrom_mem(TCS34725_ADDRESS, COMMAND_BIT | reg, 1), 'little')
-
-
-def tcs34725_read16(reg):
-    data = i2c_0.readfrom_mem(TCS34725_ADDRESS, COMMAND_BIT | reg, 2)
-    return data[1] << 8 | data[0]
-
-
-def tcs34725_init():
-    sensor_id = tcs34725_read8(ID)
-
-    tcs34725_write8(ENABLE, ENABLE_PON)
-    time.sleep(0.01)
-    tcs34725_write8(ENABLE, ENABLE_PON | ENABLE_AEN)
-
-    tcs34725_write8(ATIME, 0x00)         # Max integration time
-    tcs34725_write8(CONTROL, GAIN_4X)    # Set gain
+    def read16(self, reg):
+        data = self.i2c.readfrom_mem(self.DEVICE_ADDRESS, self.COMMAND_BIT | reg, 2)
+        return data[1] << 8 | data[0]
 
 
-def tcs34725_read_colors():
-    time.sleep(0.7)  # Wait for integration
-    c = tcs34725_read16(CDATA)
-    r = tcs34725_read16(CDATA + 2)
-    g = tcs34725_read16(CDATA + 4)
-    b = tcs34725_read16(CDATA + 6)
-    return r, g, b, c
+    def read_colors(self):
+        time.sleep(0.7)  # Wait for integration
+        c = self.read16(self.CDATA)
+        r = self.read16(self.CDATA + 2)
+        g = self.read16(self.CDATA + 4)
+        b = self.read16(self.CDATA + 6)
+        return r, g, b, c
+
+
+# i2c_0 setup
+i2c_0 = I2C(0, scl=Pin(I2C_0_SCL), sda=Pin(I2C_0_SDA), freq=400000)
+
+# Create colour sensor instance
+colour_sensor = Tcs34725(i2c_0)
+
+# Setup I2C
+i2c_1 = I2C(1, scl=Pin(I2C_1_SCL), sda=Pin(I2C_1_SDA), freq=400000)
+
+# Create servo driver isntance
+servo_driver = Pca9685(i2c_1)
+
+# The colour ranges for matching each ball colour
+# The smoothness means that they reflect a lot of light and so the RGB values are not
+# what are expected. They were worked out by testing each ball and setting close ranges
+color_ranges = {
+    ("red",   ((0.7, 0.9), (0.1, 0.2), (0.1, 0.2))),
+    ("green", ((0.3, 0.5), (0.4, 0.6), (0.1, 0.3))),
+    ("blue",  ((0.1, 0.3), (0.3, 0.5), (0.3, 0.5))),
+    ("pink",   ((0.5, 0.7), (0.15, 0.25), (0.15, 0.25 ))),
+    ("purple", ((0.3, 0.5), (0.2, 0.4), (0.2, 0.4))),
+    ("yellow",  ((0.45, 0.65), (0.25, 0.4), (0.1, 0.2)))
+}
 
 def interpolate_angle(start, end):  
     if start == end:
@@ -233,26 +226,14 @@ def interpolate_angle(start, end):
         dir = 1
         
     while a != end:
-        pca9685_set_servo_angle(0, a)
+        servo_driver.set_servo_angle(0, a)
         a = a + dir
         time.sleep(0.01)
         
 def next_ball():
-    interpolate_angle(MAX_ANGLE, MIN_ANGLE)
-    #pca9685_set_servo_angle(0, 0)
+    interpolate_angle(BALL_TOGGLE_MAX_ANGLE, BALL_TOGGLE_MIN_ANGLE)
     time.sleep(0.5)
-    interpolate_angle(MIN_ANGLE, MAX_ANGLE)
-    #pca9685_set_servo_angle(0, 40)
-
-
-color_ranges = {
-    ("red",   ((0.7, 0.9), (0.1, 0.2), (0.1, 0.2))),
-    ("green", ((0.3, 0.5), (0.4, 0.6), (0.1, 0.3))),
-    ("blue",  ((0.1, 0.3), (0.3, 0.5), (0.3, 0.5))),
-    ("pink",   ((0.5, 0.7), (0.15, 0.25), (0.15, 0.25 ))),
-    ("purple", ((0.3, 0.5), (0.2, 0.4), (0.2, 0.4))),
-    ("yellow",  ((0.45, 0.65), (0.25, 0.4), (0.1, 0.2)))
-}
+    interpolate_angle(BALL_TOGGLE_MIN_ANGLE, BALL_TOGGLE_MAX_ANGLE)
 
 
 def get_color_name(r, g, b):
@@ -262,15 +243,8 @@ def get_color_name(r, g, b):
     return "unknown"
 
 
-# i2c_0 setup on GPIO5 (SCL), GPIO4 (SDA)
-i2c_0 = I2C(0, scl=Pin(5), sda=Pin(4), freq=400000)
-
-# Setup I2C on GPIO16 (SCL), GPIO17 (SDA)
-i2c_1 = I2C(1, scl=Pin(19), sda=Pin(18), freq=400000)
-
 # Initialize the stepper motor
-stepper_motor = HalfStepMotor.frompins(
-    STEPPERI_IN1, STEPPERI_IN2, STEPPERI_IN3, STEPPERI_IN4)
+stepper_motor = StepperMotor()
 
 stepper_home_sensor = ADC(Pin(STEPPER_HOME_SENSOR_PIN))
 
@@ -295,48 +269,47 @@ def stepper_is_home():
 
 
 # Start main program
-pca9685_init()
-tcs34725_init()
-stepper_is_home_prev = stepper_is_home()
-
 try:
+    # On boot home the director arm stepper
     home_stepper()
     
-    pca9685_set_servo_angle(MIN_ANGLE, MAX_ANGLE)
+    servo_driver.set_servo_angle(BALL_TOGGLE_MIN_ANGLE, BALL_TOGGLE_MAX_ANGLE)
 
     while True:
-        stepper_sensor_value = stepper_home_sensor.read_u16()
+        # Get the raw component of each color along with the colour total
+        r_raw, g_raw, b_raw, c = colour_sensor.read_colors()
 
-        r, g, b, c = tcs34725_read_colors()
-
-        ball_detected = c > 3000
+        # Has a ball been detected
+        ball_detected = c > BALL_DETECTED_THRESHOLD
         
         if ball_detected:
-            time.sleep(1)
-            r_raw, g_raw, b_raw, c = tcs34725_read_colors()
-
+            # Convert each colour to a percetage of total colour to get correct ratio
             r = r_raw / c
             g = g_raw / c
             b = b_raw / c
 
+            # Get the ccolour name for the specified colour ratios
             color = get_color_name(r, g, b)
 
             if color != "unknown":
-                print(f"{color}")
                 if color == "red" or color == "pink" or color == "purple":
-                    stepper_motor.step_until(4096 - 170, -1)
-                else:
-                    stepper_motor.step_until(170, 1)
-                #print("{}, {}, {}, {}".format(r, g, b, c))
+                    # All balls that are of a red-ish color move to tray 1
+                    stepper_motor.step_until(TRAY_1_DIRECT_POS, STEPPER_CCW)
+                
+                else: # Green, yellow and blue
+                    # All balls that are not a red-ish color move to tray 2
+                    stepper_motor.step_until(TRAY_2_DIRECT_POS, STEPPER_CW)
                 
             else:
+              # If the colour was not detected then report it as unknown
               print("Unknown color")
               print("{}, {}, {}, {}".format(r, g, b, c))
-              stepper_motor.step_until(205)
 
+            # Release the ball from the colour sensor and get next ball if present
             next_ball()
         
         time.sleep(0.3)
 except KeyboardInterrupt:
     print("Execution stopped.")
+
 
